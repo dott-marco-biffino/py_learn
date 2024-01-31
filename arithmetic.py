f9cf1e73facdc1313f2edfc8fe90faa2dcafe345
Marco Biffino <marco.biffino@gmail.com>
2024-01-31 17:53:20 +0100

    added 'arithmetic.py' on basical arithmetics

---
+++ arithmetic.py
@@ -0,0 +1,19 @@
+# numbers and arithmetics in Python
+
+# Integer number
+
+print(3)
+
+print(10)
+
+# Floating precision number
+
+print(3.123)
+
+print(.987)
+
+# Numbers can be negative of course...
+
+# basic arithmetic infix operators: -, +, /, *, %
+
+print(str(3) + "my favorite number")
